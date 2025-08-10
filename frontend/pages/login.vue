55a540e6023544eaf14face85b1e39e5ff90e7b1
9fianl <68319010015@loeitech.ac.th>
2025-08-10 15:49:30 +0700

    jobfinal

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,28 +1,34 @@
 <template>
-  <div class="flex justify-center items-center min-h-screen bg-gradient-to-br from-gray-900 to-black">
-    <div class="bg-gray-800 p-8 rounded-xl shadow-xl w-full max-w-sm">
-      <h1 class="text-2xl font-bold text-white text-center mb-6">Login</h1>
+  <div class="flex justify-center items-center min-h-screen bg-gradient-to-br from-gray-900 to-black " style="background-image: url(https://wallpapers-clan.com/wp-content/uploads/2024/03/nezuko-glowing-eyes-demon-slayer-gif-desktop-wallpaper-preview.gif);">
+    <div class="bg-gray-800/50 backdrop-blur-sm p-8 rounded-3xl shadow-xl w-full max-w-sm">
+      <h1 class="text-2xl font-bold text-white text-center font-serif">Welcome to</h1>
+      <h1 class="text-2xl font-bold text-pink-500 text-center font-serif">P2UKaoser</h1>
+      <p class="text-sm text-gray-400 text-center mb-6 mt-2">Sign in to your account</p>
 
-      <input
-        v-model="username"
-        placeholder="Username"
-        class="input-style"
-        autocomplete="username"
-      />
+      <div class="flex flex-col">
+        <label for="" class="text-white ms-3">Username</label>
+          <input
+            v-model="username"
+            placeholder="Username"
+            class="input-style  bg-white p-3 rounded-lg border-2 border-gray-400 focus:outline-none focus:border-4 focus:border-pink-600"
+            autocomplete="username"
+          />
 
-      <input
-        v-model="password"
-        type="password"
-        placeholder="Password"
-        class="input-style"
-        autocomplete="current-password"
-      />
+        <label for="" class="text-white ms-3">Password</label>
+          <input
+            v-model="password"
+            type="password"
+            placeholder="Password"
+            class="input-style bg-white p-3 rounded-lg border-2 border-gray-400 focus:border-4 focus:outline-none focus:border-pink-600"
+            autocomplete="current-password"
+          />
+      </div>
 
       <button
         @click="handleLogin"
-        class="bg-black hover:bg-gray-700 text-white font-semibold py-2 w-full rounded-lg transition duration-200 border border-white/10 focus:outline-none focus:ring-2 focus:ring-white focus:border-white mt-4"
+        class="bg-pink-600 hover:bg-pink-500 text-white font-semibold py-2 w-full rounded-lg transition duration-200 border border-white/10 focus:outline-none focus:ring-2 focus:ring-white focus:border-white mt-4"
       >
-        Login
+        Log In
       </button>
 
       <p v-if="errorMsg" class="text-red-400 text-sm mt-4 text-center">
@@ -31,7 +37,7 @@
 
       <div class="text-center text-sm text-gray-400 mt-4">
         Don’t have an account?
-        <router-link to="/register" class="underline hover:text-white">Register</router-link>
+        <router-link to="/register" class="text-white underline hover:text-sky-500">Register here</router-link>
       </div>
     </div>
   </div>
